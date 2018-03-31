c92d11b61c084f6af9af10a05804600d07002c03
Stefan Loeffler <s.loeffler@custom-co.de>
2018-03-31 22:49:04 +0200

    added base type for technical exceptions

---
+++ src/Core.ExceptionHandling/TechnicalException.cs
@@ -0,0 +1,83 @@
+namespace CustomCode.Core.ExceptionHandling
+{
+    using System;
+
+    /// <summary>
+    /// Use this type (or derived types) whenever a technical exception has occured.
+    /// </summary>
+    /// <remarks>
+    /// Note that all technical exceptions should be displayed to the user using a generalized
+    /// "Unexpected Exception" message (hiding the real cause of the problem from the user).
+    /// </remarks>
+    public class TechnicalException : LocalizableException
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TechnicalException"/> type.
+        /// </summary>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        public TechnicalException(
+            string developerMessage = "An unexpected technical exception has occured.",
+            string userMessageResourceKey = DefaultResxKey)
+            : base(developerMessage, userMessageResourceKey)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TechnicalException"/> type.
+        /// </summary>
+        /// <param name="innerException"> The exception that cause this exception to be thrown. </param>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        public TechnicalException(
+            Exception innerException,
+            string developerMessage = "An unexpected technical exception has occured.",
+            string userMessageResourceKey = DefaultResxKey)
+            : base(innerException, developerMessage, userMessageResourceKey)
+        { }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// The .resx file key that represents a general default message for technical exceptions.
+        /// </summary>
+        private const string DefaultResxKey = "ErrorTechnicalDefaultMessage";
+
+        #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// Convert exception data to an object array that can be used via <see cref="string.Format(string, object[])"/> for
+        /// localization purposes.
+        /// </summary>
+        /// <returns> The exception's format items for localization or null. </returns>
+        public override object[] GetFormatItems()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Test.Core.ExceptionHandling/TechnicalExceptionTests.cs
@@ -0,0 +1,67 @@
+namespace CustomCode.Core.ExceptionHandling.Tests
+{
+    using System;
+    using System.Linq;
+    using Test.BehaviorDrivenDevelopment;
+    using Xunit;
+
+    [UnitTest]
+    [Category("Technical Exception")]
+    public sealed class TechnicalExceptionTests : TestCase
+    {
+        #region Callstack
+
+        private static object ThrowTechnicalException()
+        {
+            try
+            {
+                ThrowAndRethrowExceptionFromLevel2();
+                return null;
+            }
+            catch(Exception e)
+            {
+                throw new TechnicalException(e, "Level 1");
+            }
+        }
+
+        private static void ThrowAndRethrowExceptionFromLevel2()
+        {
+            try
+            {
+                ThrowExceptionFromLevel3();
+            }
+            catch(Exception e)
+            {
+                throw new TechnicalException(e, "Level 2");
+            }
+        }
+
+        private static void ThrowExceptionFromLevel3()
+        {
+            throw new TechnicalException("Level 3");
+        }
+
+        #endregion
+
+        [Fact(DisplayName = "Successfully get the technical exception's context and causing context")]
+        public void GetTechnicalExceptionContextsSuccessfully()
+        {
+            Given()
+            .When(() => ThrowTechnicalException())
+            .ThenThrow<TechnicalException>(exception =>
+                {
+                    var context = exception.Context;
+                    context.FileName.Should().EndWith($"{nameof(TechnicalExceptionTests)}.cs");
+                    context.LineNumber.Should().Be(23);
+                    context.MethodName.Should().Be($"{nameof(ThrowTechnicalException)}");
+                    context.TypeName.Should().Be($"{nameof(TechnicalExceptionTests)}");
+
+                    var causingContext = exception.CausingContexts.First();
+                    causingContext.FileName.Should().EndWith($"{nameof(TechnicalExceptionTests)}.cs");
+                    causingContext.LineNumber.Should().Be(41);
+                    causingContext.MethodName.Should().Be($"{nameof(ThrowExceptionFromLevel3)}");
+                    causingContext.TypeName.Should().Be($"{nameof(TechnicalExceptionTests)}");
+                });
+        }
+    }
+}
