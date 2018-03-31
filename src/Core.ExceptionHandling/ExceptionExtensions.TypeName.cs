766fd16c9b861e0962707634b3bbb300e90d4800
Stefan Loeffler <s.loeffler@custom-co.de>
2018-03-31 20:02:33 +0200

    added extension methods to get the name of the method that has thrown an exception

---
+++ src/Core.ExceptionHandling/ExceptionExtensions.TypeName.cs
@@ -0,0 +1,83 @@
+namespace CustomCode.Core.ExceptionHandling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    /// <summary>
+    /// Extension methods for the <see cref="Exception"/> tpye
+    /// </summary>
+    public static partial class ExceptionExtensions
+    {
+        #region Logic
+
+        /// <summary>
+        /// Gets the name of the type that has raised the specified <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="exception"> The exception to act on. </param>
+        /// <returns> The name of the type that has raised the specified <paramref name="exception"/>. </returns>
+        public static string GetTypeName(this Exception exception)
+        {
+            if (exception == null)
+            {
+                return null;
+            }
+
+            var trace = new StackTrace(exception, true);
+            var frames = trace.GetFrames();
+            if (frames?.Length > 0)
+            {
+                var method = frames[0].GetMethod();
+                return method.DeclaringType.Name;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the names of the types that have raised the specified <paramref name="exception"/>'s causing exceptions.
+        /// </summary>
+        /// <param name="exception"> The exception to act on. </param>
+        /// <returns>
+        /// The names of the types that have raised the specified <paramref name="exception"/>'s causing exceptions.
+        /// </returns>
+        public static IEnumerable<string> GetCausingTypeNames(this Exception exception)
+        {
+            var result = new List<string>();
+            foreach (var causingException in exception.GetCausingExceptions())
+            {
+                result.Add(GetTypeName(causingException));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Try to get the name of the type that has raised the specified <paramref name="exception"/>'s
+        /// causing exception if a single causing exception exists or null otherwise.
+        /// </summary>
+        /// <param name="exception"> The exception to act on. </param>
+        /// <param name="causingTypeName">
+        /// The name of the type that has raised the specified <paramref name="exception"/>'s
+        /// causing exception if a single causing exception exists or null otherwise.
+        /// </param>
+        /// <returns>
+        /// True if the specified <paramref name="exception"/>'s causing exception's type name
+        /// was found, false otherwise.
+        /// </returns>
+        public static bool TryGetCausingTypeName(this Exception exception, out string causingTypeName)
+        {
+            var causingExceptions = GetCausingExceptions(exception);
+            if (causingExceptions.Count() == 1)
+            {
+                causingTypeName = GetTypeName(causingExceptions.First());
+                return true;
+            }
+
+            causingTypeName = null;
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Test.Core.ExceptionHandling/TypeNameTests.cs
@@ -0,0 +1,65 @@
+namespace CustomCode.Core.ExceptionHandling.Tests
+{
+    using System;
+    using Test.BehaviorDrivenDevelopment;
+    using Xunit;
+
+    [UnitTest]
+    [Category("Type Names")]
+    public sealed class TypeNameTests : TestCase
+    {
+        #region Callstack
+
+        private static Exception ThrowException()
+        {
+            try
+            {
+                ThrowAndRethrowExceptionFromLevel2();
+                return null;
+            }
+            catch(Exception e)
+            {
+                return e;
+            }
+        }
+
+        private static void ThrowAndRethrowExceptionFromLevel2()
+        {
+            try
+            {
+                ThrowExceptionFromLevel3();
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Level 2", e);
+            }
+        }
+
+        private static void ThrowExceptionFromLevel3()
+        {
+            throw new Exception("Level 3");
+        }
+
+        #endregion
+
+        [Fact(DisplayName = "Successfully get the exception's type name")]
+        public void GetTypeNameSuccessfully()
+        {
+            Given(() => ThrowException())
+            .When(exception => exception.GetTypeName())
+            .Then(typeName => typeName.Should().Be($"{nameof(TypeNameTests)}"));
+        }
+
+        [Fact(DisplayName = "Successfully get the exception's causing type name")]
+        public void GetCausingTypeNameSuccessfully()
+        {
+            Given(() => ThrowException())
+            .When(exception =>
+                {
+                    exception.TryGetCausingTypeName(out var causingTypeName);
+                    return causingTypeName;
+                })
+            .Then(typeName => typeName.Should().Be($"{nameof(TypeNameTests)}"));
+        }
+    }
+}
