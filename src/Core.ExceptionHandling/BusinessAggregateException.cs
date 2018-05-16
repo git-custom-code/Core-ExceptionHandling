ec06cf1c546a3ff758c137dd816cee4048fde8fb
Stefan Loeffler <s.loeffler@custom-co.de>
2018-05-16 19:49:41 +0200

    added localizable aggregate exceptions

---
+++ src/Core.ExceptionHandling/BusinessAggregateException.cs
@@ -0,0 +1,80 @@
+namespace CustomCode.Core.ExceptionHandling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Use this type (or derived types) whenever a business aggregate exception has occured.
+    /// </summary>
+    /// <remarks>
+    /// Note that all business aggregate exceptions should contain a user friendly message text that can and
+    /// should be displayed to the application users.
+    /// </remarks>
+    public class BusinessAggregateException : BusinessException, IAggregateException
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="BusinessAggregateException"/> type.
+        /// </summary>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        public BusinessAggregateException(
+            string userMessageResourceKey,
+            string developerMessage = "An unexpected business aggregate exception has occured.")
+            : base(userMessageResourceKey, developerMessage)
+        {
+            InnerExceptions = new ReadonlyCollection<Exception>(null);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TechnicalAggregateException"/> type.
+        /// </summary>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        /// <param name="innerExceptions"> The exceptions that have caused this exception to be thrown. </param>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        public BusinessAggregateException(
+            string userMessageResourceKey,
+            IEnumerable<Exception> innerExceptions,
+            string developerMessage = "An unexpected business aggregate exception has occured.")
+            : base(userMessageResourceKey, (innerExceptions?.Count() > 0 ? innerExceptions.First() : null), developerMessage)
+        {
+            InnerExceptions = new ReadonlyCollection<Exception>(innerExceptions);
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// The .resx file key that represents a general default message for business aggregate exceptions.
+        /// </summary>
+        private const string DefaultResxKey = "ErrorBusinessAggregateDefaultMessage";
+
+        /// <summary>
+        /// Gets the exceptions that have caused this exception to be thrown.
+        /// </summary>
+        public ReadonlyCollection<Exception> InnerExceptions { get; }
+
+        #endregion
+    }
+}
---
+++ src/Core.ExceptionHandling/TechnicalAggregateException.cs
@@ -0,0 +1,80 @@
+namespace CustomCode.Core.ExceptionHandling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Use this type (or derived types) whenever a technical aggregate exception has occured.
+    /// </summary>
+    /// <remarks>
+    /// Note that all technical aggregate exceptions should be displayed to the user using a generalized
+    /// "Unexpected Exception" message (hiding the real cause of the problem from the application users).
+    /// </remarks>
+    public class TechnicalAggregateException : TechnicalException, IAggregateException
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TechnicalAggregateException"/> type.
+        /// </summary>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        public TechnicalAggregateException(
+            string developerMessage = "An unexpected technical aggregate exception has occured.",
+            string userMessageResourceKey = DefaultResxKey)
+            : base(developerMessage, userMessageResourceKey)
+        {
+            InnerExceptions = new ReadonlyCollection<Exception>(null);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TechnicalAggregateException"/> type.
+        /// </summary>
+        /// <param name="innerExceptions"> The exceptions that have caused this exception to be thrown. </param>
+        /// <param name="developerMessage">
+        /// The exception's message.
+        /// Note that this message is not localized and should not be displayed to the application users.
+        /// This message is meant to contain developer relevant informations and can be used for logging purposes.
+        /// </param>
+        /// <param name="userMessageResourceKey">
+        /// A .resx key that can be used to localize the exception's message.
+        /// Note that this message should be displayed to the application users and therefore can or should be localized
+        /// if the application requires it. This message should not contain any informations that is irrelevant or
+        /// confusing for the application users.
+        /// </param>
+        public TechnicalAggregateException(
+            IEnumerable<Exception> innerExceptions,
+            string developerMessage = "An unexpected technical aggregate exception has occured.",
+            string userMessageResourceKey = DefaultResxKey)
+            : base((innerExceptions?.Count() > 0 ? innerExceptions.First() : null), developerMessage, userMessageResourceKey)
+        {
+            InnerExceptions = new ReadonlyCollection<Exception>(innerExceptions);
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// The .resx file key that represents a general default message for technical aggregate exceptions.
+        /// </summary>
+        private const string DefaultResxKey = "ErrorTechnicalAggregateDefaultMessage";
+
+        /// <summary>
+        /// Gets the exceptions that have caused this exception to be thrown.
+        /// </summary>
+        public ReadonlyCollection<Exception> InnerExceptions { get; }
+
+        #endregion
+    }
+}
